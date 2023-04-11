d9cef8bb444c735442de90b92bfca702df34abfd
Samuel Mann <samuelcmann@gmail.com>
2023-04-11 10:09:53 +0200

    show timer in table, modify voice

--- script.sh
+++ script.sh
@@ -3,14 +3,34 @@
 loop_duration=$1
 pause_duration=$2
 
-while true; do
-  # Play a sound to signal the start of the loop
-  echo start
-  say -v Kyoko start
-  sleep $loop_duration
+# Set the text attributes for the header row
+tput bold
+printf "%-20s %-20s %-20s\n" "Remaining Time" "Total Time Elapsed" "Number of Loops"
+tput sgr0
 
-  # Play a sound to signal the end of the loop
-  echo stop
+count=0
+remaining_time=$loop_duration
+total_time_elapsed=0
+while true; do
+ say -v Kyoko start
+  while [ $remaining_time -gt 0 ]; do
+    # Print the current values in the table with a carriage return
+    printf "\r%-20s %-20s %-20s" "$remaining_time" "$total_time_elapsed" "$count"
+
+    # Decrement the remaining time counter and sleep for 1 second
+    remaining_time=$((remaining_time - 1))
+    total_time_elapsed=$((total_time_elapsed + 1))
+    sleep 1
+  done
+
+  # Increment the loop counter
+  count=$((count + 1))
+
+  # Print the final values in the table after each interval
+  printf "\r%-20s %-20s %-20s" "0" "$total_time_elapsed" "$count"
+
   say -v Kyoko stop
+
+  remaining_time=$loop_duration
   sleep $pause_duration
 done
